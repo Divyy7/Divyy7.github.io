bae94124331ddf68cd6825493aa7b25625f78db4
Divyy7 <52523647+Divyy7@users.noreply.github.com>
2019-07-28 15:30:45 +0530

    Add files via upload

---
+++ javascript/login.js
@@ -0,0 +1,14 @@
+
+
+function validate(){
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+
+	if ( username == "7283722" && password == "726472"){
+		alert ("Login successfull");
+	}
+	else{
+		alert("Incorrect Username or password. Please try Again");
+	} }
+		
+		
